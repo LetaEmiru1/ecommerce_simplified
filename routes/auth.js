ca3c921447d51173f941331985f0909cb7880f58
Leta Malimo <letaemiruleta@gmail.com>
2025-12-22 20:06:08 +0300

    implement user registration and add environment variables

--- routes/auth.js
+++ routes/auth.js
@@ -1,38 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs'); // Tool for hashing passwords
-const User = require('../models/User'); // Import the Schema we made in Step 3
+const User = require('../models/User'); // Import the Schema
+const jwt = require('jsonwebtoken');
 
 // @route   POST /api/auth/register
 // @desc    Register a new user
 router.post('/register', async (req, res) => {
   try {
     // 1. Destructure the data coming from the user (frontend)
-    // The data is stored in 'req.body'
     const { username, email, password } = req.body;
 
     // 2. CHECK IF USER EXISTS
-    // Use the User model to look for a user with this email.
-    // Syntax hint: await User.findOne({ field: value })
-    const userExists = await User.findOne({ /* ??? FILL THIS IN ??? */ });
+    const userExists = await User.findOne({ email: email});
 
     if (userExists) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
     // 3. HASH THE PASSWORD
-    // We never save plain text passwords.
-    // Use bcrypt.genSalt(10) to create a salt.
-    // Use bcrypt.hash(password, salt) to create the hash.
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(/* ??? FILL THIS IN ??? */ salt);
+    const hashedPassword = await bcrypt.hash(password,salt);
 
     // 4. CREATE THE USER
     // Create a new user instance using the Model
     const newUser = new User({
       username: username,
       email: email,
-      password: /* ??? WHICH VARIABLE GOES HERE? ??? */
+      password: hashedPassword
     });
 
     // 5. SAVE TO DB
