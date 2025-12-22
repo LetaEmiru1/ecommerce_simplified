2ac9671d76c55bd8ed6bbf479eab1b28b6d57362
Leta Malimo <letaemiruleta@gmail.com>
2025-12-22 21:39:09 +0300

    login functionality

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs'); // Tool for hashing passwords
 const User = require('../models/User'); // Import the Schema
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 // @route   POST /api/auth/register
 // @desc    Register a new user
@@ -23,7 +24,6 @@ router.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password,salt);
 
     // 4. CREATE THE USER
-    // Create a new user instance using the Model
     const newUser = new User({
       username: username,
       email: email,
@@ -37,8 +37,33 @@ router.post('/register', async (req, res) => {
     res.status(201).json({ message: 'User registered successfully' });
 
   } catch (error) {
-    res.status(500).json({ message: 'Server Error', error: error.message });
+    return res.status(500).json({ message: 'Server Error', error: error.message });
   }
 });
+//@route POST/api/auth/login
+//@desc authenticate user and get token
+router.post('/login', async (req, res) => {
+    try{
+        const {email, password} = req.body;
+        const user = await User.findOne({email: email});
+        if(!user){
+            return res.status(400).json({message: 'Invalid credentials'});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({message: 'Invalid credentials'});
+        }
+        //token generation
+        const payload = {id: user._id}; //payload is the data inside the badge (jwt ?) usually user id
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '1h'})
+
+        //RESPOND
+        res.json(
+            {token, user : {id: user._id, username: user.username, email: user.email}}
+        );
+    } catch (error) {
+        res.status(500).json({message: 'Server Error', error: error.message});
+    }
+})
 
 module.exports = router;
